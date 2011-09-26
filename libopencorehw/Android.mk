2fb342767a7902b2fae489cb0dd6952aac0f7843
Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>
2011-09-26 18:36:14 +0530

    build: standardize building conditionally for qcom chipsets
    Replace QCOM_TARGET_PRODUCT usage with make functions provided by
    build/core/utils.mk to build conditionally for qcom chipsets
    
    Change-Id: Ic7da313627ff3fcf0f3895ef0f3c822c0b5a77e8
    CRs-Fixed: 298335

--- libopencorehw/Android.mk
+++ libopencorehw/Android.mk
@@ -8,10 +8,10 @@ include external/opencore/Config.mk
 LOCAL_C_INCLUDES := $(PV_INCLUDES)\
                     hardware/msm7k/libgralloc-qsd8k
 
-ifneq (, $(filter msm7201a_ffa msm7201a_surf msm7627a msm7627_ffa msm7627_surf qsd8250_ffa qsd8250_surf qsd8650a_st1x, $(QCOM_TARGET_PRODUCT)))
+ifeq ($(call is-board-platform-in-list,msm7627a msm7627_surf),true)
   LOCAL_SRC_FILES := android_surface_output_msm72xx.cpp
 endif
-ifneq (, $(filter msm7630_surf msm7630_1x msm7630_fusion msm8660_surf msm8660_csfb, $(QCOM_TARGET_PRODUCT)))
+ifeq ($(call is-board-platform-in-list,msm7630_surf msm7630_fusion msm8660),true)
   LOCAL_SRC_FILES := android_surface_output_msm7x30.cpp
 endif
 
