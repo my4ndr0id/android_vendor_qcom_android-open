9cab064fbb9093c3a66d63a5dcdb021e5ff2ae64
Nagender Telkar <ntelkar@codeaurora.org>
2011-10-07 13:49:23 -0600

    build: Updating LOCAL_MODULE_TAGS with optional tag.
    Change-Id: Ib9153f312d3a85b8b5b11d216095bc518a58a484
    CRs-Fixed: 310616

--- amploader/Android.mk
+++ amploader/Android.mk
@@ -20,6 +20,8 @@ LOCAL_SRC_FILES := amploader.c
 
 LOCAL_MODULE := amploader
 
+LOCAL_MODULE_TAGS := optional
+
 LOCAL_PRELINK_MODULE := false
 include $(BUILD_EXECUTABLE)
 
--- pvomx/Android.mk
+++ pvomx/Android.mk
@@ -1,8 +1,14 @@
 ifneq ($(BUILD_WITHOUT_PV),true)
 LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
 
-$(call add-prebuilt-files, ETC, 01_qcomm_omx.cfg)
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := 01_qcomm_omx.cfg
+LOCAL_BUILT_MODULE_STEM := 01_qcomm_omx.cfg
+LOCAL_MODULE_SUFFIX := $(suffix 01_qcomm_omx.cfg)
+LOCAL_MODULE := $(basename 01_qcomm_omx.cfg)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS :=  ETC
+include $(BUILD_PREBUILT)
 
 include vendor/qcom/android-open/pvomx/omx_core_plugin/Android.mk
 endif
