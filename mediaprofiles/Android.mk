d314b91a72af78160d618a5f7a5084150e92d4b2
Kevin Chan <ktchan@codeaurora.org>
2011-11-18 02:41:07 -0800

    camera: Adding mediaprofile for 8960
    Enable front camera on ICS
    
    Change-Id: I600bf2e57384c9c7b5740f92702ecf1753fd9a36

--- mediaprofiles/Android.mk
+++ mediaprofiles/Android.mk
@@ -3,6 +3,21 @@ LOCAL_PATH := $(my-dir)
 ########################
 include $(CLEAR_VARS)
 
+ifeq ($(call is-board-platform,msm8960),true)
+
+LOCAL_MODULE := media_profiles.xml
+
+LOCAL_MODULE_TAGS := optional
+
+# This will install the file in /system/etc
+#
+LOCAL_MODULE_CLASS := ETC
+
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+
+include $(BUILD_PREBUILT)
+endif
+
 ifeq ($(call is-board-platform,msm8660),true)
 
 LOCAL_MODULE := media_profiles.xml
