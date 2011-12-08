8f0fcfbdaeb157d24a03ff26b7f3741fb10a5927
Riaz Rahaman <riazr@codeaurora.org>
2011-12-08 20:01:50 +0530

    mediaprofiles: Add target specific mediaprofiles
    Add mediaprofiles for 7627a target which has different media
    profiles compared to other targets.
    
    Change-Id: I50037f4b59cdc04cb095f2f7384f17d9e59d1574
    CRs-Fixed: 321002

--- mediaprofiles/Android.mk
+++ mediaprofiles/Android.mk
@@ -1,39 +1,15 @@
+ifeq ($(call is-board-platform-in-list,msm8960 msm8660 msm7627a),true)
+
 LOCAL_PATH := $(my-dir)
 
 ########################
 include $(CLEAR_VARS)
 
-ifeq ($(call is-board-platform,msm8960),true)
-
-LOCAL_MODULE := media_profiles.xml
-
-LOCAL_MODULE_TAGS := optional
-
-# This will install the file in /system/etc
-#
-LOCAL_MODULE_CLASS := ETC
-
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
-
-include $(BUILD_PREBUILT)
-endif
-
-ifeq ($(call is-board-platform,msm8660),true)
-
-LOCAL_MODULE := media_profiles.xml
-
-LOCAL_MODULE_TAGS := optional
-
-# This will install the file in /system/etc
-#
-LOCAL_MODULE_CLASS := ETC
-
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
-
-include $(BUILD_PREBUILT)
-endif
-
 ifeq ($(call is-board-platform,msm7627a),true)
+LOCAL_SRC_FILES := media_profiles_7627a.xml
+else ifeq ($(call is-board-platform-in-list,msm8960 msm8660),true)
+LOCAL_SRC_FILES := media_profiles.xml
+endif
 
 LOCAL_MODULE := media_profiles.xml
 
@@ -42,8 +18,6 @@ LOCAL_MODULE_TAGS := optional
 # This will install the file in /system/etc
 #
 LOCAL_MODULE_CLASS := ETC
-
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
 
 include $(BUILD_PREBUILT)
 endif
